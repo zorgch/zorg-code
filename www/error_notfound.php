962ae0b137f96ce4d4620b683e4033dc43c607ef
Oliver Raduner <github@raduner.ch>
2014-07-23 21:07:56 +0200

    Using older HTTP response header code for 404 error page

--- www/error_notfound.php
+++ www/error_notfound.php
@@ -1,6 +1,7 @@
 <?php
 	
-	http_response_code(404);
+	// PHP >= 5.4 only: http_response_code(404);
+	header('X-PHP-Response-Code: 404', true, 404); // PHP >= 4.3
 	
 	require_once($_SERVER['DOCUMENT_ROOT'].'/includes/main.inc.php');
 	
