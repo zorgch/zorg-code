25bbc289e29afb4703b61b1c9408741b945591c3
Oliver Raduner <github@raduner.ch>
2016-02-09 01:42:50 +0100

    Attempted fix #2 for PHP White Screen of Death on zorg.ch after last commit

--- www/index.php
+++ www/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/includes/main.inc.php');
+//require_once($_SERVER['DOCUMENT_ROOT'].'/includes/main.inc.php');
 
 if (!isset($_GET['tpl']) && !isset($_GET['word'])) {
 	$_GET['tpl'] = 23;
@@ -7,4 +7,3 @@ if (!isset($_GET['tpl']) && !isset($_GET['word'])) {
 
 include_once($_SERVER['DOCUMENT_ROOT'].'/includes/smarty.inc.php');
 $smarty->display("file:main.html");
-?>
