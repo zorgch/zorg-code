9a2aa94f8253e7d559a058e348541e2d9439379a
Oliver Raduner <github@raduner.ch>
2019-01-03 11:28:41 +0100

    Fixed "blank page"-redirect after editing a Template

--- www/actions/tpleditor_close.php
+++ www/actions/tpleditor_close.php
@@ -1,20 +1,16 @@
-<?
+<?php
 require_once( __DIR__ .'/../includes/main.inc.php');
 require_once( __DIR__ .'/../includes/tpleditor.inc.php');
 
-	
-	tpleditor_unlock($_GET['tplupd']);
-	
-	if (!$_GET['location']) {
-		if ($_GET['tplupd'] == 'new') {
-			$_GET['location'] = base64_encode('/?');
-		}else{
-			$_GET['location'] = base64_encode("/?tpl=$_GET[tplupd]");
-		}
-	}
-	
-	unset($_GET['tpleditor']);
-	unset($_GET['tplupd']);
-	 
-	header('Location: '.base64_decode($_GET['location']));
-	die();
+tpleditor_unlock($_GET['tplupd']);
+
+if (empty($_GET['location'])) {
+	if ($_GET['tplupd'] == 'new') $_GET['location'] = base64_encode('/?');
+	else $_GET['location'] = base64_encode('/tpl/'.$_GET['tplupd']);
+}
+
+unset($_GET['tpleditor']);
+unset($_GET['tplupd']);
+
+header('Location: '.base64_decode($_GET['location']));
+exit;
