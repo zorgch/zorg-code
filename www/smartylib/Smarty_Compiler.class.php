29ddfdeadfd501be6eac92413a54cea80c6855c6
Florian Bentele <florian.bentele@alea-iacta.ch>
2019-11-03 16:51:43 +0100

    fixes deprecated smarty function (create_function)

--- www/smartylib/Smarty_Compiler.class.php
+++ www/smartylib/Smarty_Compiler.class.php
@@ -262,14 +262,19 @@ class Smarty_Compiler extends Smarty {
         reset($this->_folded_blocks);
 
         /* replace special blocks by "{php}" */
-        $source_content = preg_replace_callback($search, create_function ('$matches', "return '"
-                                       . $this->_quote_replace($this->left_delimiter) . 'php'
-                                       . "' . str_repeat(\"\n\", substr_count('\$matches[1]', \"\n\")) .'"
-                                       . $this->_quote_replace($this->right_delimiter)
-                                       . "';")
-                                       , $source_content);
+        // TODO: fix this deprecated code in smarty
+	    $callback = function($matches) {
+		    return $this->_quote_replace($this->left_delimiter) . 'php' .
+		           str_repeat('\n\\', substr_count('\\'.$matches[1], '\n\\')) .
+		                              //str_repeat("\n", substr_count($matches[1], "\n"))
+		                              $this->_quote_replace($this->right_delimiter);
+	    };
 
-        /* Gather all template tags. */
+        $source_content = preg_replace_callback($search, $callback, $source_content);
+
+
+
+	    /* Gather all template tags. */
         preg_match_all("~{$ldq}\s*(.*?)\s*{$rdq}~s", $source_content, $_match);
         $template_tags = $_match[1];
         /* Split content by template tags to obtain non-template content. */
