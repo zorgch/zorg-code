d511f798c23b4a2e7f975743d90f08121a7a68b4
Florian Bentele <fbentele@gmail.com>
2019-12-28 23:49:22 +0100

    optimized get parameter validation

--- www/profil.php
+++ www/profil.php
@@ -10,8 +10,11 @@ require_once( __DIR__ .'/models/core.model.php');
 /**
  * Validate GET-Parameters
  */
-if (!empty($_GET['regcode'])) $userRegcode = sanitize_userinput($_GET['regcode']);
+$doAction    = null;
+$user_id     = null;
+$userRegcode = null;
 if (!empty($_GET['do']) && !is_numeric($_GET['do'])) $doAction = sanitize_userinput($_GET['do']);
+if (!empty($_GET['regcode'])) $userRegcode = sanitize_userinput($_GET['regcode']);
 if (!empty($_GET['user_id']) && is_numeric($_GET['user_id'])) $user_id = sanitize_userinput($_GET['user_id']);
 
 //=============================================================================
@@ -29,9 +32,6 @@ Messagesystem::execActions();
 /**
  * Userlist anzeigen
  */
-$doAction    = isset( $doAction ) ? $doAction : null;
-$user_id     = isset( $user_id ) ? $user_id : null;
-$userRegcode = isset( $userRegcode ) ? $userRegcode : null;
 if ( !$doAction && !$user_id && !$userRegcode )
 {
 	$smarty->display('file:layout/head.tpl');
@@ -163,7 +163,7 @@ if (!empty($user_id))
 	$htmlOutput .= '<img src="'.$user->userImage($user_id, 1).'" style="width: 100%;max-width: 100%;">';
 
 	/** User Addle */
-	if ($user->id > 0 && $user_id != $user->id && $rs['addle'])
+	if (isset($user->id) && $user->id > 0 && $user_id != $user->id && $rs['addle'])
 	{
 		$sidebarHtml .= '<h3>Addle</h3>
 		<form action="/addle.php?show=overview&do=new" method="post">
