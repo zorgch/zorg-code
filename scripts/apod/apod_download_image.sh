7fbaaaab649b0dcd86f2ea0f3937c3913b0be2e2
oliveratgithub <github@raduner.ch>
2020-10-13 22:50:28 +0200

    Fix APOD script for new dir structure on FUCKUP

--- scripts/apod/apod_download_image.sh
+++ scripts/apod/apod_download_image.sh
@@ -14,39 +14,48 @@
 # Usage via command line on xoli using a ssh shell
 # ---
 # - must be run via "sudo" for proper permissions!
-# - 2 parameters are required:
+# - 3 parameters are required:
 #   1) Gallery Pic Image-ID
 #   2) APOD Image-URL (wrapped in quotes "")
+#   3) the path to zorg's /data/ directory
 # 
 # Example:
-# $ sudo /usr/local/bin/apod/apod_download_image.sh [galleryImageID] 'https://apod.nasa.gov/apod/image/APOD_IMAGE_ID/APOD_IMAGE_NAME.jpg'
+# $ sudo ./apod_download_image.sh [galleryImageID] 'https://apod.nasa.gov/apod/image/APOD_IMAGE_ID/APOD_IMAGE_NAME.jpg' '/absolute/path/to/zorg/data'
 #
 # @author IneX
 # @package zorg
 # @subpackage Scripts
-# @version 1.0
+# @version 2.0
 # @since 1.0 <inex> 14.06.2019 Script added
+# @since 2.0 <inex> 30.10.2020 Added input for /data/-dir for compatibility on zorg FUCKUP
 #==========
 
 # Input parameter validation
 if [ -z "$1" ]; then
-  printf "[WARN] Missing input 1 the Gallery Image-ID. Use:\nscript.sh galleryImageID 'https://apod.nasa.gov/apod/image/image_id/image_name.jpg'\n"
+  printf "[WARN] Missing input 1 the Gallery Image-ID. Use:\napod_download_image.sh galleryImageID 'https://apod.nasa.gov/apod/image/image_id/image_name.jpg' '/absolute/path/to/zorg/data'\n"
   exit 2
 else
   gallery_picid_input="$1"
 fi
 if [ -z "$2" ]; then
-  printf "[WARN] Missing input 2 with APOD URL. Use:\nscript.sh galleryImageID 'https://apod.nasa.gov/apod/image/image_id/image_name.jpg'\n"
+  printf "[WARN] Missing input 2 with APOD URL. Use:\napod_download_image.sh galleryImageID 'https://apod.nasa.gov/apod/image/image_id/image_name.jpg' '/absolute/path/to/zorg/data'\n"
   exit 2
 else
   apod_url_input="$2"
+fi
+if [ -z "$3" ]; then
+  printf "[WARN] Missing input 3 with zorg /data/ path. Use:\napod_download_image.sh galleryImageID 'https://apod.nasa.gov/apod/image/image_id/image_name.jpg' '/absolute/path/to/zorg/data'\n"
+  exit 2
+else
+  zorg_data_dir="${3%/}"
+  # dir=${x%/} => Uses suffix removal parameter expansion construct to remove trailing slash - if there is one
 fi
 
 # Define variables
 apod_download_url=$apod_url_input
 pic_id=$gallery_picid_input
-tmp_dirpath="/var/data/temp/"
-apod_gallery_dir="/var/data/gallery/41/"
+tmp_dirpath="$zorg_data_dir/temp/"
+apod_gallery_dir="$zorg_data_dir/gallery/41/"
 file_extension="jpg"
 gallery_pic="pic_$pic_id"
 gallery_tn="tn_$pic_id"
@@ -74,4 +83,4 @@ printf "Cleanup. Removing temporary downloaded file: $tmp_dirpath$tmp_filename\n
 rm $tmp_dirpath$tmp_filename
 
 # Done
-printf "DONE! Check it out:\nhttp://zorg.ch/gallery/$pic_id\n"
+printf "DONE! Check it out:\nhttps://zorg.ch/gallery/$pic_id\n"
