d93f3278dc53cc42cccbb7a75a55f391d595f966
oliveratgithub <github@raduner.ch>
2020-10-20 14:44:27 +0200

    Fixes some Chess warnings

--- www/scripts/chess_overview.php
+++ www/scripts/chess_overview.php
@@ -9,18 +9,20 @@ include_once dirname(__FILE__).'/../includes/chess.inc.php';
 
 global $db, $user, $smarty;
 
+/** Instantiate Chess Game Class */
+$chess = new Chess();
 
-// my games
-$smarty->assign("my_games", Chess::my_games());
+/** my games */
+$smarty->assign('my_games', $chess->my_games());
 
-
-// users for new game
-$e = $db->query("SELECT id, username FROM user WHERE chess='1' AND id!='$user->id' ORDER BY username ASC", __FILE__, __LINE__);
+/** users for new game */
+$e = $db->query('SELECT id, username FROM user WHERE chess="1" AND id<>'.$user->id.' ORDER BY username ASC', __FILE__, __LINE__, 'SELECT Chess Players'); // chess = ENUM(string)
 $user_ids = array();
 $user_names = array();
-while ($d = $db->fetch($e)) {
+while ($d = $db->fetch($e))
+{
 	$user_ids[] = $d['id'];
 	$user_names[] = $d['username'];
 }
-$smarty->assign("user_ids", $user_ids);
-$smarty->assign("user_names", $user_names);
+$smarty->assign('user_ids', $user_ids);
+$smarty->assign('user_names', $user_names);
