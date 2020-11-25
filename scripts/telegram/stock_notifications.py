69da946b51b6d0a6fd5973526ab7ed4e2b81009b
oliveratgithub <github@raduner.ch>
2020-11-25 20:34:20 +0100

    Stocks symbol notification-script for Telegram

---
+++ scripts/telegram/stock_notifications.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ~~~~~
+# == Setup ==
+# % pip install [--user] requests
+# % pip install [--user] schedule
+# % pip install [--user] yfinance
+#
+# Make Python-script executable:
+# % chmod +x ./stock_notifications.py
+#
+# == Usage ==
+# one time:
+# % python3 ./stock_notifications.py "BTC-USD
+#
+# running as a service:
+# % nohup python3 ./stock_notifications.py "TSLA" $
+#
+# ~~~~~
+# Original source: https://codeburst.io/indian-stock-market-price-notifier-bot-telegram-92e376b0c33a
+# Resources:
+#  - Symbol: https://finance.yahoo.com/quote/TSLA?p=TSLA
+#  - YF for Python: https://pypi.org/project/fix-yahoo-finance/0.1.30/
+#  - YF4P docu: https://aroussi.com/post/python-yahoo-finance
+#  - Telegram Bot API: https://core.telegram.org/bots/api#sendmessage
+# ~~~~~
+import sys
+import requests
+import time
+import schedule
+import datetime
+import json
+import yfinance as yf
+import urllib.parse
+import botconfigs as bot
+
+def getStock():
+    if len(bot.token) > 40 and len(bot.chat) > 5:
+        bot_token=bot.token
+        bot_chatID=bot.chat
+    else:
+        print("Missing botconfigs!")
+        quit()
+
+    if len(sys.argv) <= 1:
+        print("Missing a valid symbol as parameter. Use something like 'TSLA' or 'BTC-USD'...")
+        quit()
+    else:
+        symbol=sys.argv[1]
+
+    currency=''
+    price=0
+    prev_price=0
+
+    #ticker=pdr.get_data_yahoo(symbol, start=datetime.datetime(2020, 11, 25), end=datetime.datetime(2020, 12, 31))
+    ticker_meta=yf.Ticker(symbol)
+    ticker_dict=ticker_meta.info
+    ticker_json=json.dumps(ticker_dict)
+    ticker_json=json.loads(ticker_json)
+    if ticker_json["currency"] != '':
+        currency=ticker_json["currency"]
+
+    ticker=yf.download(symbol, period="1d")
+    #DEBUG: print(ticker)
+    if prev_price == 0:
+        prev_price=(ticker["Open"][0]).round(2)
+    else:
+        prev_price=price
+    price=(ticker["Close"][0]).round(2)
+    if prev_price == 0 or price == prev_price:
+        price_change_str = ''
+        price_diff_str = ''
+    elif price > prev_price:
+        price_diff = (price-prev_price).round(2)
+        price_change_str = "surged"
+        price_diff_str = "("+price_change_str+" +"+str("{0:,.2f}".format(price_diff)).replace(',', '\'')+")"
+    else:
+        price_diff = (prev_price-price).round(2)
+        price_change_str = "dropped"
+        price_diff_str = "("+price_change_str+" -"+str("{0:,.2f}".format(price_diff)).replace(',', '\'')+")"
+
+    message=symbol+" @ *"+currency+" "+str("{0:,.2f}".format(price)).replace(',', '\'')+"* "+price_diff_str
+    message=message.replace("-","\-")
+    message=message.replace("+","\+")
+    message=message.replace(".","\.")
+    message=message.replace("(","\(")
+    message=message.replace(")","\)")
+    message=message.replace("?","\?")
+    message=message.replace("^","\^")
+    message=message.replace("$","\$")
+    message=urllib.parse.quote_plus(message)
+    #DEBUG: print(message)
+    send='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=MarkdownV2&text=' + message
+    #DEBUG: print(send)
+    response=requests.get(send)
+    #DEBUG: print(response)
+import threading
+while True:
+    getStock()
+    # Sleep 12 hours = 43200 seconds
+    time.sleep(43200)
