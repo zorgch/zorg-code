555dddceae9952f55a7361697b4ac6ea910af2cf
Oliver <github@raduner.ch>
2021-05-18 21:33:29 +0200

    Fixes some Use of undefined constant PHP Warnings

--- www/includes/events.inc.php
+++ www/includes/events.inc.php
@@ -3,7 +3,7 @@
  * Events Funktionen
  *
  * Beinhaltet die Events-Klasse und deren Methoden, welche für die Events benötigt werden
- * 
+ *
  * Diese Klassen benutzen folgende Tabellen aus der DB:
  * - events
  * - events_to_user
@@ -36,80 +36,62 @@ include_once INCLUDES_DIR.'googleapis.inc.php';
  * @package	zorg\Events
  */
 class Events
-{	
-	static function getEvent($event_id) {
+{
+	static function getEvent($event_id)
+	{
 		global $db;
-		
-		$sql =	
-			"
-			SELECT
-				*
+
+		$sql = 'SELECT *
 				, UNIX_TIMESTAMP(startdate) AS startdate
 				, UNIX_TIMESTAMP(enddate) AS enddate
 				, UNIX_TIMESTAMP(reportedon_date) AS reportedon_date
-			FROM 
-			events
-			WHERE
-			id = ".$event_id."
-			"
-		; 
-		 
-		$result = $db->query($sql, __FILE__, __LINE__);
-		
+				FROM events
+				WHERE id='.$event_id;
+
+		$result = $db->query($sql, __FILE__, __LINE__, __METHOD__);
+
 		return $db->fetch($result);
 	}
-	
-	static function getEventNewest() {
+
+	static function getEventNewest()
+	{
 		global $db;
-		
-		$sql =	
-			"
-			SELECT
-				*
+
+		$sql = 'SELECT *
 				, UNIX_TIMESTAMP(startdate) AS startdate
 				, UNIX_TIMESTAMP(enddate) AS enddate
 				, UNIX_TIMESTAMP(reportedon_date) AS reportedon_date
-			FROM 
-			events
-			ORDER BY reportedon_date DESC
-			LIMIT 0,1
-			"
-		; 
-		 
-		$result = $db->query($sql, __FILE__, __LINE__);
-		
+				FROM events
+				ORDER BY reportedon_date DESC
+				LIMIT 0,1';
+
+		$result = $db->query($sql, __FILE__, __LINE__, __METHOD__);
+
 		return $db->fetch($result);
 	}
-	
-	static function getEvents($year) {
+
+	static function getEvents($year)
+	{
 		global $db;
-		
+
 		$events = array();
-		
-		$sql =	
-			"
-			SELECT
-				*
-				, UNIX_TIMESTAMP(reportedon_date) AS reportedon_date
-			FROM 
-			events
-			WHERE
-			DATE_FORMAT(startdate, '%Y') = ".$year."
-			ORDER BY startdate ASC, enddate ASC
-			"
-		; 
-		 
-		$result = $db->query($sql, __FILE__, __LINE__);
-		
+
+		$sql = 'SELECT *, UNIX_TIMESTAMP(reportedon_date) AS reportedon_date
+				FROM events
+				WHERE DATE_FORMAT(startdate, "%Y") = '.$year.'
+				ORDER BY startdate ASC, enddate ASC';
+
+		$result = $db->query($sql, __FILE__, __LINE__, __METHOD__);
+
 		while($rs = $db->fetch($result)) {
 			array_push($events, $rs);
 		}
-		
+
 		return $events;
 	}
 
 	/**
-	 * Bevorstehende Events suchen
+	 * Bevorstehende Events suchen.
 	 * Findet alle Events der nächsten 7 Tagen und gibt diese als PHP-Array zurück
 	 *
 	 * @version 1.1
@@ -128,7 +110,7 @@ class Events
 
 		$events = array();
 		$user_id = isset($user->id) ? $user->id : 0;
-		$sql = 'SELECT 
+		$sql = 'SELECT
 				  e.id,
 				  e.name,
 				  e.startdate,
@@ -137,7 +119,7 @@ class Events
 				FROM events e
 					LEFT JOIN comments c ON (c.board = "e" AND c.thread_id = e.id)
 					LEFT JOIN comments_unread cu ON (cu.user_id = '.$user_id.' AND cu.comment_id = c.id)
-				WHERE 
+				WHERE
 					e.enddate >= NOW() AND (e.startdate BETWEEN (NOW() - INTERVAL 7 DAY) AND NOW())
 					OR e.startdate BETWEEN NOW() AND (NOW() + INTERVAL 5 DAY)
 				GROUP by e.id
@@ -147,95 +129,64 @@ class Events
 		while($rs = $db->fetch($result)) {
 			$events[] = $rs;
 		}
-		
 		return $events;
 	}
 
-	static function getNumNewEvents() {
+	static function getNumNewEvents()
+	{
 		global $db, $user;
-		
-		if(isset($user->lastlogin) && $user->lastlogin > 0) {
-			$sql =	
-				"
-				SELECT
-				*
-				FROM 
-				events
-				WHERE
-				UNIX_TIMESTAMP(reportedon_date) > ".$user->lastlogin."
-				"
-			;
-			
-			return $db->num($db->query($sql, __FILE__, __LINE__));
+
+		if(isset($user->lastlogin) && $user->lastlogin > 0)
+		{
+			$sql = 'SELECT * FROM events WHERE UNIX_TIMESTAMP(reportedon_date) > '.$user->lastlogin;
+
+			return $db->num($db->query($sql, __FILE__, __LINE__, __METHOD__));
 		} else {
 			return 0;
 		}
 	}
-	
-	static function getVisitors($event_id) {
+
+	static function getVisitors($event_id)
+	{
 		global $db;
 		$visitors = array();
-		
-		$sql = 
-			"
-			SELECT
-			*
-			from events_to_user e
-			where e.event_id = ".$event_id." 
-			"
-		;
-		$result = $db->query($sql, __FILE__, __LINE__);
+
+		$sql = 'SELECT * FROM events_to_user WHERE event_id = '.$event_id;
+		$result = $db->query($sql, __FILE__, __LINE__, __METHOD__);
 		while ($rs = $db->fetch($result)) {
 			array_push($visitors, $rs);
 		}
-		
+
 		return $visitors;
 	}
-	
-	static function getYears() {
+
+	static function getYears()
+	{
 		global $db;
-		
 		$years = array();
-		
-		$sql =	
-			"
-			SELECT
-				DATE_FORMAT(startdate, '%Y') as year
-			FROM 
-			events
-			GROUP BY year
-			ORDER BY year ASC
-			"
-		; 
-		 
-		$result = $db->query($sql, __FILE__, __LINE__);
-		
+
+		$sql = 'SELECT DATE_FORMAT(startdate, "%Y") as year FROM events GROUP BY year ORDER BY year ASC';
+		$result = $db->query($sql, __FILE__, __LINE__, __METHOD__);
 		while($rs = $db->fetch($result)) {
 			array_push($years, $rs['year']);
 		}
-		
+
 		return $years;
 	}
-	
-	static function hasJoined($user_id, $event_id) {
+
+	static function hasJoined($user_id, $event_id)
+	{
 		global $db;
-		
-		$sql = 
-			"
-			SELECT 
-			* 
-			FROM events_to_user 
-			WHERE user_id = ".$user_id."
-			AND event_id = ".$event_id
-		;
-		$result = $db->query($sql, __FILE__, __LINE__);
-		
+
+		$sql = 'SELECT * FROM events_to_user WHERE user_id = '.$user_id.' AND event_id = '.$event_id;
+		$result = $db->query($sql, __FILE__, __LINE__, __METHOD__);
+
 		return $db->fetch($result);
 	}
-	
-	
+
 	/**
-	 * Returns the Title of an Event based on a given ID
+	 * Returns the Title of an Event based on a given ID.
+	 *
 	 * @author IneX
 	 * @version 1.1
 	 * @since 1.0 `18.08.2012` `IneX` method added
@@ -249,20 +200,19 @@ class Events
 	{
 		global $db;
 
-		$sql = 'SELECT id, name FROM events WHERE id = '.$event_id; 
+		$sql = 'SELECT id, name FROM events WHERE id = '.$event_id;
 		$rs = $db->fetch($db->query($sql, __FILE__, __LINE__, __METHOD__));
 
 		return (!empty($rs) && false !== $rs ? remove_html($rs['name']) : '');
 	}
 
-
 	/**
-	 * Returns the Link to an Event based on a given ID
+	 * Returns the Link to an Event based on a given ID.
 	 * URL Format: /event/[year]/[month]/[day]/[event-id|eventname]
+	 *
 	 * @author IneX
-	 * @date 20.09.2018
 	 * @version 1.0
-	 * @since 1.0 function added
+	 * @since 1.0 `20.09.2018` `IneX` Method added
 	 *
 	 * @see index.php
 	 * @param int $event_id
@@ -272,7 +222,7 @@ class Events
 	{
 		global $db;
 
-		$sql = 'SELECT DATE_FORMAT(startdate,"%Y/%m/%d") as date_path, id FROM events WHERE id='.$event_id; 
+		$sql = 'SELECT DATE_FORMAT(startdate,"%Y/%m/%d") as date_path, id FROM events WHERE id='.$event_id;
 		$rs = $db->fetch($db->query($sql, __FILE__, __LINE__, __METHOD__));
 		if ($rs) {
 			$eventLink = sprintf('/event/%s/%d', $rs['date_path'], $rs['id']);
@@ -281,44 +231,44 @@ class Events
 			return false;
 		}
 	}
-	
-	
+
 	/**
 	 * Returns a specific amount of the Description of an Event
-	 * @author IneX
-	 * @date 18.08.2012
 	 *
-	 * @param $text string
-	 * @param $length int
+	 * @TODO use text_width() from util.inc.php
+	 *
+	 * @author IneX
+	 * @version 1.0
+	 * @since 1.0 `18.08.2012` `IneX` Method added
+	 *
+	 * @param string $text
+	 * @param int $length
 	 * @return String mit Event Kurzbeschreibung
 	 */
-	static function getEventExcerpt($text, $length=50) {
-		
+	static function getEventExcerpt($text, $length=50)
+	{
 		$text = strip_tags($text);
-		
+
 		// was macht das?
-		$pattern = "(((\w|\d|[√§√∂√º√®√©√†√Æ√™])(\w|\d|\s|[√§√∂√º√®√©√†√Æ√™]|[\.,-_\"'?!^`~])[^\\n]+)(\\n|))";
+		$pattern = "(((\w|\d|[√§√∂√º√®√©√†√Æ√™])(\w|\d|\s|[√§√∂√º√®√©√†√Æ√™]|[\.,-_\"'?!^`~])[^\\n]+)(\\n|))"; // FIXME Umlauts are b0rken
 		preg_match($pattern, $text, $out);
 		if(strlen($out[1]) > $length)
 		{
 			$out[1] = substr($out[1], 0, $length);
 		}
 		if(strlen($out[1]) == 0) return '---';
-		
+
 		return $out[1];
 	}
 }
-
 
 /**
  * Upcoming Events
  *
  * @author IneX
- * @date 12.06.2018
  * @version 1.0
- * @since 1.0
- * @package zorg
- * @subpackage Events
+ * @since 1.0 `12.06.2018` `IneX` Class added
+ * @package zorg\Events
  */
 class UpcomingEvent
 {
@@ -346,7 +296,7 @@ class UpcomingEvent
 		if (is_numeric($starts_in_hours))
 		{
 			$nextEvent = $this->getUpcomingEvent($starts_in_hours);
-			
+
 			if ($nextEvent)
 			{
 				/** If we have lat+lng, send an event... */
@@ -386,39 +336,35 @@ class UpcomingEvent
 	private function getUpcomingEvent($hours_until_start)
 	{
 		global $db, $googleMapsApi;
-		try {
-			$sql = 'SELECT name, location, UNIX_TIMESTAMP(startdate) time
-					FROM events
-					WHERE startdate >= DATE_ADD(NOW(), INTERVAL '.($hours_until_start-1).' HOUR)
-						AND startdate < DATE_ADD(NOW(), INTERVAL '.($hours_until_start).' HOUR)
-					ORDER BY startdate ASC
-					LIMIT 1';
-			$result = $db->query($sql, __FILE__, __LINE__, __METHOD__);
-			$event = $db->fetch($result);
-			if (DEVELOPMENT) error_log("[DEBUG] Event Query Result:\n\r".print_r($event,true));
 
-			if (!empty($event['name']))
+		$sql = 'SELECT name, location, UNIX_TIMESTAMP(startdate) time
+				FROM events
+				WHERE startdate >= DATE_ADD(NOW(), INTERVAL '.($hours_until_start-1).' HOUR)
+					AND startdate < DATE_ADD(NOW(), INTERVAL '.($hours_until_start).' HOUR)
+				ORDER BY startdate ASC
+				LIMIT 1';
+		$result = $db->query($sql, __FILE__, __LINE__, __METHOD__);
+		$event = $db->fetch($result);
+		if (DEVELOPMENT) error_log("[DEBUG] Event Query Result:\n\r".print_r($event,true));
+
+		if (!empty($event['name']))
+		{
+			if (DEVELOPMENT) error_log("[DEBUG] getUpcomingEvent()\n\r" . print_r($event, true));
+			$geolocation = $googleMapsApi->geocode($event['location']);
+			if(!empty($geolocation))
 			{
-				if (DEVELOPMENT) error_log("[DEBUG] getUpcomingEvent()\n\r" . print_r($event, true));
-				$geolocation = $googleMapsApi->geocode($event['location']);
-				if(!empty($geolocation))
-				{
-					if (DEVELOPMENT) error_log('[DEBUG] $googleMapsApi->geocode()'."\n\r" . print_r($geolocation, true));
-					$event['lat'] = $geolocation['lat'];
-					$event['lng'] = $geolocation['lng'];
-					//return $event;
-				}/* else {
-					error_log( t('error-googlemapsapi-geocode', 'event', [__METHOD__, __LINE__]) );
-					return NULL;
-				}*/
-				return $event;
-			} else {
-				error_log( t('error-upcoming-event', 'event', [__METHOD__, __LINE__, $hours_until_start]) );
+				if (DEVELOPMENT) error_log('[DEBUG] $googleMapsApi->geocode()'."\n\r" . print_r($geolocation, true));
+				$event['lat'] = $geolocation['lat'];
+				$event['lng'] = $geolocation['lng'];
+				//return $event;
+			}/* else {
+				error_log( t('error-googlemapsapi-geocode', 'event', [__METHOD__, __LINE__]) );
 				return NULL;
-			}
-		} catch (Exception $e) {
-			error_log($e->getMessage());
-			return $e->getMessage();
+			}*/
+			return $event;
+		} else {
+			error_log( t('error-upcoming-event', 'event', [__METHOD__, __LINE__, $hours_until_start]) );
+			return NULL;
 		}
 	}
 }
