15ed676237d25408917098e7b507753a3e217650
Oliver <github@raduner.ch>
2021-05-18 22:54:53 +0200

    Adds input escaping for $_GET

--- www/error_notfound.php
+++ www/error_notfound.php
@@ -2,4 +2,5 @@
 require_once dirname(__FILE__).'/includes/main.inc.php';
 
 http_response_code(404); // Set response code 404 (not found)
+$smarty->assign('tplroot', ['menus' => ['zorg']]);
 $smarty->display(SMARTY_404PAGE_TPL);
