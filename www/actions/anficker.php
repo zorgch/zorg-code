b2bc6ce90e8f0dda9bb7a73da1c4fdce2e3c91e1
Oliver <github@raduner.ch>
2024-01-21 13:00:57 +0100

    Resolves Bug 667
    [Anficker] Anficks werden x-mal gespeichert

--- www/actions/anficker.php
+++ www/actions/anficker.php
@@ -6,26 +6,29 @@
  * @package zorg\Games\Anficker
  */
 // Includes --------------------------------------------------------------------
-require_once dirname(__FILE__).'/../includes/main.inc.php';
+require_once __DIR__.'/../includes/config.inc.php';
 require_once INCLUDES_DIR.'anficker.inc.php';
 
+/** Input validation */
+$doAction = filter_input(INPUT_POST, 'do', FILTER_DEFAULT, FILTER_REQUIRE_SCALAR) ?? null; // $_POST['do']
+$trainSpresim = filter_input(INPUT_POST, 'spresim-trainieren', FILTER_VALIDATE_BOOLEAN) ?? false; // $_POST['spresim-trainieren']
+$anfickId = filter_input(INPUT_POST, 'anfick_id', FILTER_VALIDATE_INT) ?? 0; // $_POST['anfick_id']
+$anfickScore = filter_input(INPUT_POST, 'note', FILTER_VALIDATE_INT) ?? 0; // $_POST['note']
+$anfick = htmlentities(filter_input(INPUT_POST, 'text', FILTER_SANITIZE_FULL_SPECIAL_CHARS)) ?? null; // $_POST['text']
+$returnUrl = '/tpl/175';
 
 // Anficken -------------------------------------------------------------------
-if(isset($_POST['do']) && $_POST['do'] == 'anficken')
-{		
+if($user->is_loggedin() && $doAction === 'anficken')
+{
 	/**
 	 * Benoten NUR wenn spresim-trainieren gewählt wird
 	 * und eine Note vorhanden ist
 	 */
-	if (isset($_POST['spresim-trainieren']) && (isset($_POST['note']) && is_numeric($_POST['note']) && $_POST['note'] > 0))
-	{
-		Anficker::vote($_POST['anfick_id'], $_POST['note']);
-	}
+	if ($trainSpresim && $anfickId > 0 && $anfickScore > 0) Anficker::vote($anfickId, $anfickScore);
 
-	$textEscaped = htmlentities(addslashes($_POST['text']));
+	if (!empty($anfick)) Anficker::addAnfick(max(0, $user->id), $anfick, $trainSpresim);
 
-	Anficker::addAnfick(max(0, $user->id), $textEscaped, $_POST['spresim-trainieren']);
-
-	header("Location: /tpl/175?del=no&spresimtrainieren=".$_POST['spresim-trainieren']."#anficker");	
-	exit;
+	$returnUrl .= '?del=no&spresimtrainieren='.$trainSpresim.'#anficker';
 }
+header('Location: '.$returnUrl);
+exit;
