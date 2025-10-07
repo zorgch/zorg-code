32a7df68f6a419736b81d6b1907553b913410cd3
Oliver <github@r>
2025-10-08 01:20:41 +0200

    Fixes SQL error when updating `show_comments`

--- public/actions/show_tpl_comments.php
+++ public/actions/show_tpl_comments.php
@@ -1,14 +1,32 @@
 <?php
-require_once dirname(__FILE__).'/../includes/usersystem.inc.php';
-	
+/**
+ * Show / Hide Comments
+ *
+ * @package zorg\Usersystem
+ */
+
+/**
+ * File includes
+ */
+require_once __DIR__.'/../includes/config.inc.php';
+
+/** User not logged in? Error in his face! */
+if (!$user->is_loggedin()) {
+	http_response_code(403); // Set response code 403 (Forbidden)
+	user_error('Access denied', E_USER_ERROR);
+}
+
+/** Validate passed Parameters */
+$set_usershowcomments = (string)filter_input(INPUT_GET, 'usershowcomments', FILTER_VALIDATE_INT) ?? null; // $_GET['usershowcomments'], 0=hide / 1=show
+
 /** Comments ein/ausblenden */
-if (isset($_GET['usershowcomments']) && $_GET['usershowcomments'] != $user->show_comments)
+if (isset($set_usershowcomments) && $set_usershowcomments != $user->show_comments)
 {
-	$db->query('UPDATE user SET show_comments="'.$_GET['usershowcomments'].'" WHERE id='.$user->id, __FILE__, __LINE__, 'UPDATE SET show_comments');
-	$user->show_comments = $_GET['usershowcomments'];
+	$db->update('user', $user->id, ['show_comments' => $set_usershowcomments], __FILE__, __LINE__, 'UPDATE user SET show_comments');
+	$user->show_comments = $set_usershowcomments;
 }
 
 /** Redirect to previous page */
 unset($_GET['usershowcomments']);
 header('Location: /?'.url_params());
-die();
+exit;
